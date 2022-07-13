15054dedab9cfdae249743505d3ee69a5a9e89f9
Kamelia Karam <kamelia_karam@hotmail.com>
2022-07-13 15:39:47 +0700

    created post function to create a house

--- controllers/houses.js
+++ controllers/houses.js
@@ -1,5 +1,6 @@
 const express = require ('express')
 const router = express.Router()
+const Houses= require ('../models/houses')
 
 router.get ('/', (req, res) => {
     res.render('houses/list', {
@@ -17,11 +18,12 @@ router.get ('/create', (req, res) => {
     }
 })
 
-router.get ('/:id', (req, res) => {
-    res.render('houses/one', {
-        user: req.user
-    })
-})
+// router.get ('/:id', (req, res) => {
+//     let x = Houses.findById()
+//     res.render('houses/one', {
+//         user: req.user
+//     })
+// })
 
 router.get ('/:id/edit', (req, res) => {
     if (req.isAuthenticated()) {
@@ -33,11 +35,21 @@ router.get ('/:id/edit', (req, res) => {
     }
 })
 
-router.post('/', (req, res) => {
-    if (req.isAuthenticated()) {
-        res.send('hello from houses')
-    } else {
-        res.redirect('/auth/login')
+router.post('/', async (req, res, next) => {
+    try {
+        if (req.isAuthenticated()) {
+    req.body.host=req.user._id
+    let newhouse = await Houses.create(req.body)
+        if (newhouse) {
+            res.redirect('/houses/:id')
+        } else {
+            throw new Error ('incorrect')
+        }
+    } else{
+     res.redirect('/auth/login')
+    } 
+    } catch (err) {
+        next (err)
     }
 })
 
